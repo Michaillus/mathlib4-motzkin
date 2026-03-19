6ecd978d50c376c881acbe41f8479f351eae3b78
Ansar Azhdarov <78869764+raja-19@users.noreply.github.com>
2026-03-19 21:53:03 +0100

    progress from after friday
    i am on v4.28.0, may not compile on latest rc

--- Counterexamples/Motzkin.lean
+++ Counterexamples/Motzkin.lean
@@ -1,123 +1,115 @@
-/-
-Copyright (c) 2025 Jeremy Tan. All rights reserved.
-Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Jeremy Tan, Heather Macbeth
--/
-import Mathlib.Tactic.LinearCombination
-import Mathlib.Tactic.Positivity
 import Mathlib
 
-/-!
-# The Motzkin polynomial
+namespace MvPolynomial
 
-The Motzkin polynomial is a well-known counterexample: it is nonnegative everywhere, but not
-expressible as a polynomial sum of squares.
+theorem totalDegree_pow_eq {R : Type u} {σ : Type u_1} [CommSemiring R] [IsReduced R]
+    (f : MvPolynomial σ R) (n : ℕ) : (f ^ n).totalDegree = n * f.totalDegree := by
+  sorry
 
-This file contains a proof of the first property (nonnegativity).
+end MvPolynomial
 
-TODO: prove the second property.
--/
+open MvPolynomial
 
 variable {K : Type*} [CommRing K] [LinearOrder K] [IsStrictOrderedRing K]
 
-/-- The **Motzkin polynomial** is nonnegative.
-This bivariate polynomial cannot be written as a sum of squares. -/
-lemma motzkin_polynomial_nonneg (x y : K) :
-    0 ≤ x ^ 4 * y ^ 2 + x ^ 2 * y ^ 4 - 3 * x ^ 2 * y ^ 2 + 1 := by
-  by_cases hx : x = 0
-  · simp [hx]
-  have h : 0 < (x ^ 2 + y ^ 2) ^ 2 := by positivity
-  refine nonneg_of_mul_nonneg_left ?_ h
-  have H : 0 ≤ (x ^ 3 * y + x * y ^ 3 - 2 * x * y) ^ 2 * (1 + x ^ 2 + y ^ 2)
-    + (x ^ 2 - y ^ 2) ^ 2 := by positivity
-  linear_combination H
-
-
-
-open MvPolynomial
-/- noncomputable def motzkin : MvPolynomial (Fin 2) K := -/
-/-   let x := X 0 -/
-/-   let y := X 1 -/
-/-   x ^ 4 * y ^ 2 + x ^ 2 + y ^ 4 - 3 * x ^ 2 * y ^ 2 + 1 -/
+notation "⟨x" n "y" m "⟩" => (Finsupp.single 0 n + Finsupp.single 1 m)
 
 noncomputable def motzkin : MvPolynomial (Fin 2) K :=
-  (monomial (Finsupp.single 0 4 + Finsupp.single 1 2) 1) + 
-  (monomial (Finsupp.single 0 2 + Finsupp.single 1 4) 1) + 
-  (monomial (Finsupp.single 0 2 + Finsupp.single 1 2) (-3)) + 
-  (monomial (Finsupp.single 0 0) 1)
+  (monomial ⟨x 4 y 2⟩ 1) + (monomial ⟨x 2 y 4⟩ 1) - (monomial ⟨x 2 y 2⟩ 3) + (monomial 0 1)
 
-lemma motzkin_polynomial_ne_sos (s : Finset (MvPolynomial (Fin 2) K)) :
-    motzkin ≠ ∑ g ∈ s, g ^ 2 := by
-  intro h
+lemma motzkin_ne_sos {ι : Type*} (s : Finset ι) (g : ι → MvPolynomial (Fin 2) K) :
+    motzkin ≠ ∑ i ∈ s, g i ^ 2 := by
+  by_contra! h1
 
-  /- have h_nox (n : ℕ) (hn : n > 0) : (coeff (Finsupp.single (0: Fin 2) n) g) = 0 := by -/
-  /- have h_nox : ∀ n : ℕ, n > 0 → ∀ g ∈ s, (coeff (Finsupp.single (0: Fin 2) n) g) = 0 := by -/
-  /-   intro n hn g hg -/
-  /-   sorry -/
-
-     
-
-  let m : Fin 2 →₀ ℕ := Finsupp.single 0 2 + Finsupp.single 1 2
-  let c : K := coeff m motzkin
-  have : c = -3 := by
-    unfold c m motzkin
-    simp [Finsupp.ext_iff, MvPolynomial.coeff_one]
-
-  have s_nonempty : s.Nonempty := by
-    by_contra! hs
-    have h1 := hs ▸ h
-    rw [Finset.sum_empty, MvPolynomial.ext_iff] at h1
-    specialize h1 0
-    simp [motzkin] at h1
-  
-  have motzkin_xk_zero : ∀ n > 0, motzkin.coeff (Finsupp.single 0 n) = (0 : K) := by
-    intro n hn
-    simp [motzkin, Finsupp.ext_iff, MvPolynomial.coeff_one]
-    omega
-
-  have g_xk_zero : ∀ g ∈ s, ∀ k : ℕ, k > 0 →
-      g.coeff (Finsupp.single 0 k) = 0 := by
-
-    let k_sup := s.sup' s_nonempty (MvPolynomial.degreeOf 0)
-    have h1 := MvPolynomial.ext_iff.mp h
-    specialize h1 (Finsupp.single 0 (2 * k_sup))
-    suffices k_sup = 0 by
-      intro g hg k hk_pos
-      by_contra! hk
-      have : k_sup ≥ k := by
-        simp [k_sup]
-        use g
-        refine ⟨hg, ?_⟩
-        unfold degreeOf
-        rw [MvPolynomial.degrees_def]
-
-        sorry
-        
-
-
-
-
-      sorry
-
-
-    by_contra! hsup
-    rw [motzkin_xk_zero (2 * k_sup) (by positivity)] at h1
-
-
-
+  have h2 {i : ι} (hi : i ∈ s) (m : _) (hm : (g i).coeff m ≠ 0) : m 0 + m 1 ≤ 3 := by
+    rw [← mem_support_iff] at hm
+    revert m
+    rw [← Finset.sup_le_iff]
+    revert i
+    rw [← Finset.sup_le_iff]
     sorry
-  
-  have coeff_yk_zero : ∀ g ∈ s, ∀ k : ℕ, k > 0 →
-      g.coeff (Finsupp.single (1 : Fin 2) k) = 0 := by
-    sorry
-  
-  have coeff_geq3_zero : ∀ g ∈ s, ∀ a : ℕ, a ≥ 0 → ∀ b : ℕ,
-      g.coeff (Finsupp.single 0 a + Finsupp.single 1 a) = 0 := by
+    
+/-
+    suffices h2 : (g i).totalDegree ≤ 3 by
+      apply le_trans _ h2
+      conv =>
+        right; rw [totalDegree]; right; intro s
+        simp [Finsupp.sum_of_support_subset s (Finset.subset_univ s.support)]
+      apply Finset.le_sup (mem_support_iff.mpr hm)
+
+    -- MvPolynomial.coeff_eq_zero_of_totalDegree_lt
+    /- rw [← degreeOf_monomial_eq m 0 (by decide : 1 ≠ 0)] -/
+    /- rw [← degreeOf_monomial_eq m 1 (by decide : 1 ≠ 0)] -/
+
+    
+
+
+    /- rw [← Nat.mul_le_mul_left_iff zero_lt_two, ← totalDegree_pow_eq] -/
+    clear m hm
+    revert i
+    rw [← Finset.sup_le_iff]
+    unfold totalDegree
+    conv =>
+      left; right; intro i; right; intro s
+      simp [Finsupp.sum_of_support_subset s (Finset.subset_univ s.support)]
+  -/
+
+
+    /- simp only [totalDegree, Nat.reduceMul, Finset.sup_le_iff, mem_support_iff, ne_eq] -/
+    /- intro i hi m hm -/
+    /- push_neg at hm -/
+    /- simp [Finsupp.sum_of_support_subset m (Finset.subset_univ m.support)] -/
+    /- contrapose! hm -/
+
+    /- have h2 : s.Nonempty := sorry -/
+    /- obtain ⟨j, h3, h4⟩ := s.exists_mem_eq_sup h2 fun i ↦ (g i).totalDegree -/
+    /- rw [h4] -/
+
+  have h3 {i : ι} (hi : i ∈ s) (m : _) (hm : (g i).coeff m ≠ 0) : 0 < m 0 ↔ 0 < m 1 := by
     sorry
 
+  let t : Set (Fin 2 →₀ ℕ) := {⟨x 2 y 1⟩, ⟨x 1 y 2⟩, ⟨x 1 y 1⟩, 0}
+  have h4 : t = {m | m 0 + m 1 ≤ 3 ∧ (0 < m 0 ↔ 0 < m 1)} := by
+    ext m
+    simp only [Set.mem_insert_iff, Set.mem_singleton_iff, Set.mem_setOf_eq, t]
+    constructor
+    · rintro (rfl | rfl | rfl | rfl)
+      <;> simp 
+    · rintro ⟨h4, h5⟩
+      suffices (m 0 = 2 ∧ m 1 = 1) ∨ (m 0 = 1 ∧ m 1 = 2) ∨
+          (m 0 = 1 ∧ m 1 = 1) ∨ (m 0 = 0 ∧ m 1 = 0) by
+        simpa [Finsupp.ext_iff]
+      have : m 0 ≤ 3 := by omega
+      have : m 1 ≤ 3 - m 0 := by omega
+      interval_cases (m 0)
+      <;> interval_cases (m 1)
+      <;> (first | contradiction | decide)
 
+  have h5 : ∀ i ∈ s, ∃ a b c d, g i = (monomial ⟨x 2 y 1⟩ a) + (monomial ⟨x 1 y 2⟩ b) +
+      (monomial ⟨x 1 y 1⟩ c) + (monomial 0 d) := by
+    intro i hi
+    refine ⟨(g i).coeff ⟨x 2 y 1⟩, (g i).coeff ⟨x 1 y 2⟩, (g i).coeff ⟨x 1 y 1⟩, (g i).coeff 0, ?_⟩
+    rw [MvPolynomial.ext_iff]
+    intro m
+    by_cases! h5 : m ∈ t
+    · simp only [Set.mem_insert_iff, Set.mem_singleton_iff, t] at h5 
+      rcases h5 with rfl | rfl | rfl | rfl
+      <;> simp [Finsupp.ext_iff] 
+    · have h6 : (g i).coeff m = 0 := by
+        simp only [h4, Set.mem_setOf_eq] at h5
+        contrapose! h5
+        refine ⟨h2 hi m h5, h3 hi m h5⟩
+      simp only [Set.mem_insert_iff, Set.mem_singleton_iff, not_or, t] at h5
+      simp [h6, h5, eq_comm]
+  
+  have h6 : ∀ i ∈ s, (g i ^ 2).coeff ⟨x 2 y 2⟩ = (g i).coeff ⟨x 1 y 1⟩ ^ 2 := by
+    intro i hi
+    obtain ⟨a, b, c, d, h6⟩ := h5 i hi
+    simp only [h6, pow_two, mul_add, add_mul, monomial_mul, coeff_add, coeff_monomial]
+    simp [Finsupp.ext_iff]
 
-  sorry
-
-
-
+  replace h1 := congr(($h1).coeff ⟨x 2 y 2⟩)
+  rw [coeff_sum, Finset.sum_congr rfl h6] at h1
+  have : 0 ≤ motzkin (K := K).coeff ⟨x 2 y 2⟩ := by simp [h1, Finset.sum_nonneg, sq_nonneg]
+  have : 0 > motzkin (K := K).coeff ⟨x 2 y 2⟩ := by simp [motzkin, coeff_one, Finsupp.ext_iff]
+  order
