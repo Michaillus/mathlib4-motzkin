71760faaa3cc4872285cbd571ed891d9086cce1f
michailus <mihail.ab3@gmail.com>
2026-03-20 17:24:26 +0100

    Changed names of lemmas

--- Counterexamples/Motzkin.lean
+++ Counterexamples/Motzkin.lean
@@ -19,9 +19,9 @@ noncomputable def motzkin : MvPolynomial (Fin 2) K :=
 
 lemma motzkin_ne_sos {ι : Type*} (s : Finset ι) (g : ι → MvPolynomial (Fin 2) K) :
     motzkin ≠ ∑ i ∈ s, g i ^ 2 := by
-  by_contra! h1
+  by_contra! motzkin_sos
 
-  have h2 {i : ι} (hi : i ∈ s) (m : _) (hm : (g i).coeff m ≠ 0) : m 0 + m 1 ≤ 3 := by
+  have total_degree_le_3 {i : ι} (hi : i ∈ s) (m : _) (hm : (g i).coeff m ≠ 0) : m 0 + m 1 ≤ 3 := by
     rw [← mem_support_iff] at hm
     revert m
     rw [← Finset.sup_le_iff]
@@ -65,17 +65,17 @@ lemma motzkin_ne_sos {ι : Type*} (s : Finset ι) (g : ι → MvPolynomial (Fin 
     /- obtain ⟨j, h3, h4⟩ := s.exists_mem_eq_sup h2 fun i ↦ (g i).totalDegree -/
     /- rw [h4] -/
 
-  have h3 {i : ι} (hi : i ∈ s) (m : _) (hm : (g i).coeff m ≠ 0) : 0 < m 0 ↔ 0 < m 1 := by
+  have x_iff_y {i : ι} (hi : i ∈ s) (m : _) (hm : (g i).coeff m ≠ 0) : 0 < m 0 ↔ 0 < m 1 := by
     sorry
 
   let t : Set (Fin 2 →₀ ℕ) := {⟨x 2 y 1⟩, ⟨x 1 y 2⟩, ⟨x 1 y 1⟩, 0}
-  have h4 : t = {m | m 0 + m 1 ≤ 3 ∧ (0 < m 0 ↔ 0 < m 1)} := by
+  have possible_mon : t = {m | m 0 + m 1 ≤ 3 ∧ (0 < m 0 ↔ 0 < m 1)} := by
     ext m
     simp only [Set.mem_insert_iff, Set.mem_singleton_iff, Set.mem_setOf_eq, t]
     constructor
     · rintro (rfl | rfl | rfl | rfl)
       <;> simp 
-    · rintro ⟨h4, h5⟩
+    · rintro ⟨motzkin_sos, h2⟩
       suffices (m 0 = 2 ∧ m 1 = 1) ∨ (m 0 = 1 ∧ m 1 = 2) ∨
           (m 0 = 1 ∧ m 1 = 1) ∨ (m 0 = 0 ∧ m 1 = 0) by
         simpa [Finsupp.ext_iff]
@@ -85,31 +85,31 @@ lemma motzkin_ne_sos {ι : Type*} (s : Finset ι) (g : ι → MvPolynomial (Fin 
       <;> interval_cases (m 1)
       <;> (first | contradiction | decide)
 
-  have h5 : ∀ i ∈ s, ∃ a b c d, g i = (monomial ⟨x 2 y 1⟩ a) + (monomial ⟨x 1 y 2⟩ b) +
+  have g_general_form : ∀ i ∈ s, ∃ a b c d, g i = (monomial ⟨x 2 y 1⟩ a) + (monomial ⟨x 1 y 2⟩ b) +
       (monomial ⟨x 1 y 1⟩ c) + (monomial 0 d) := by
     intro i hi
     refine ⟨(g i).coeff ⟨x 2 y 1⟩, (g i).coeff ⟨x 1 y 2⟩, (g i).coeff ⟨x 1 y 1⟩, (g i).coeff 0, ?_⟩
     rw [MvPolynomial.ext_iff]
     intro m
-    by_cases! h5 : m ∈ t
-    · simp only [Set.mem_insert_iff, Set.mem_singleton_iff, t] at h5 
-      rcases h5 with rfl | rfl | rfl | rfl
+    by_cases! g_general_form : m ∈ t
+    · simp only [Set.mem_insert_iff, Set.mem_singleton_iff, t] at g_general_form
+      rcases g_general_form with rfl | rfl | rfl | rfl
       <;> simp [Finsupp.ext_iff] 
     · have h6 : (g i).coeff m = 0 := by
-        simp only [h4, Set.mem_setOf_eq] at h5
-        contrapose! h5
-        refine ⟨h2 hi m h5, h3 hi m h5⟩
-      simp only [Set.mem_insert_iff, Set.mem_singleton_iff, not_or, t] at h5
-      simp [h6, h5, eq_comm]
+        simp only [possible_mon, Set.mem_setOf_eq] at g_general_form
+        contrapose! g_general_form
+        refine ⟨total_degree_le_3 hi m g_general_form, x_iff_y hi m g_general_form⟩
+      simp only [Set.mem_insert_iff, Set.mem_singleton_iff, not_or, t] at g_general_form
+      simp [h6, g_general_form, eq_comm]
   
   have h6 : ∀ i ∈ s, (g i ^ 2).coeff ⟨x 2 y 2⟩ = (g i).coeff ⟨x 1 y 1⟩ ^ 2 := by
     intro i hi
-    obtain ⟨a, b, c, d, h6⟩ := h5 i hi
+    obtain ⟨a, b, c, d, h6⟩ := g_general_form i hi
     simp only [h6, pow_two, mul_add, add_mul, monomial_mul, coeff_add, coeff_monomial]
     simp [Finsupp.ext_iff]
 
-  replace h1 := congr(($h1).coeff ⟨x 2 y 2⟩)
-  rw [coeff_sum, Finset.sum_congr rfl h6] at h1
-  have : 0 ≤ motzkin (K := K).coeff ⟨x 2 y 2⟩ := by simp [h1, Finset.sum_nonneg, sq_nonneg]
+  replace motzkin_sos := congr(($motzkin_sos).coeff ⟨x 2 y 2⟩)
+  rw [coeff_sum, Finset.sum_congr rfl h6] at motzkin_sos
+  have : 0 ≤ motzkin (K := K).coeff ⟨x 2 y 2⟩ := by simp [motzkin_sos, Finset.sum_nonneg, sq_nonneg]
   have : 0 > motzkin (K := K).coeff ⟨x 2 y 2⟩ := by simp [motzkin, coeff_one, Finsupp.ext_iff]
   order
